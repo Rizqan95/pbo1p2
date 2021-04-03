1c7f54fd13480809f7860bdaf2bd71ec3c80c2e6
Rizqan95 <richi.usman@gmail.com>
2021-04-03 15:25:20 +0800

    meemanggil dai banyak folder

---
+++ model/Mahasiswa.java
@@ -0,0 +1,10 @@
+package model;
+
+public class Mahasiswa{
+public static void tampilanInfo(){
+System.out.println();
+System.out.println("Data Mahasiswa");
+System.out.println("NPM : 19631105");
+System.out.println("Nama : Syarifuddin");
+}
+}
